7108ceabc3c26b70b23849c44a17d4544c0439fa
Mubasher <21694@student.dorset-college.ie>
2020-12-01 13:34:42 +0000

    firstr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int peopleInLine = 90;
+        int peopleOnRide = 40;
+        int time = 0;
+
+        while (peopleInLine <= 600){
+            peopleInLine -= peopleOnRide;
+            peopleInLine+=63;
+            time +=5;
+        }
+        System.out.println(time);
     }
 }
